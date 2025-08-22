3ac25fec98bde0b2d5e06835383b9a4d6ea7ab26
gbakes <gbakes89@gmail.com>
2025-08-22 09:27:26 +0200

    latest changes

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,19 +1,20 @@
 return {
-	"ThePrimeagen/harpoon",
-	lazy = false,
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
-	config = function()
-		require("harpoon").setup({
-			menu = {
-				width = math.floor(vim.api.nvim_win_get_width(0) * 0.6),
-				height = 12,
-			},
-		})
+  "ThePrimeagen/harpoon",
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    require("harpoon").setup({
+      menu = {
+        width = math.floor(vim.api.nvim_win_get_width(0) * 0.9),
+        height = 20,
+      },
+    })
 
-		-- Custom highlight groups for harpoon
-		vim.api.nvim_set_hl(0, "HarpoonWindow", { bg = "#282828", fg = "#ebdbb2" })
-		vim.api.nvim_set_hl(0, "HarpoonBorder", { fg = "#fabd2f" })
-	end,
+    -- Custom highlight groups for harpoon
+    vim.api.nvim_set_hl(0, "HarpoonWindow", { bg = "#282828", fg = "#ebdbb2" })
+    vim.api.nvim_set_hl(0, "HarpoonBorder", { fg = "#fabd2f" })
+  end,
 }
+
